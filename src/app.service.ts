b13339cea58b1ac78a6cd53681032c76031f7d7a
Abhinav Sorate <abhinavsorate01@gmail.com>
2023-04-17 23:18:29 +0530

    setup microservice

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserEvent } from './events/create-user.evet';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  handleUserCreated(data: CreateUserEvent) {
+    console.log('handlerUserCreated - COMMUNICATIONS', data);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>( //setting as microservice
+    AppModule,
+    {
+      transport: Transport.TCP, //setting TCP as transport, also can use kafka
+    },
+  );
+  await app.listen();
 }
 bootstrap();
